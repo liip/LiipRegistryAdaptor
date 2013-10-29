a79ad902e64d525f2533d44b9255bf1d18bfae94
Patrick Jezek <patrick@jezek.ch>
2013-10-29 14:14:15 +0100

    Implemented es_clean_up: removed assertion as it is to the lib to test. using getIndex from adapter.

--- src/Liip/Registry/Adaptor/Lucene/ElasticaAdaptor.php
+++ src/Liip/Registry/Adaptor/Lucene/ElasticaAdaptor.php
@@ -356,9 +356,6 @@ class ElasticaAdaptor implements AdaptorInterface
      */
     public function deleteType($indexName, $typeName)
     {
-        Assertion::string($indexName, 'indexName has to be a string!');
-        Assertion::string($typeName, 'typeName has to be a string!');
-
         $client = $this->getClient();
         $index = $client->getIndex($indexName);
         $type = $index->getType($typeName);
@@ -377,10 +374,6 @@ class ElasticaAdaptor implements AdaptorInterface
      */
     public function getTypeCount($indexName, $typeName, $query = '')
     {
-        Assertion::string($indexName, 'indexName has to be a string!');
-        Assertion::string($typeName, 'typeName has to be a string!');
-        Assertion::string($query, 'query has to be a string!');
-
         $client = $this->getClient();
         $index = $client->getIndex($indexName);
         $type = $index->getType($typeName);
@@ -398,11 +391,7 @@ class ElasticaAdaptor implements AdaptorInterface
      */
     public function getTypeMapping($indexName, $typeName)
     {
-        Assertion::string($indexName, 'indexName has to be a string!');
-        Assertion::string($typeName, 'typeName has to be a string!');
-
-        $client = $this->getClient();
-        $index = $client->getIndex($indexName);
+        $index = $this->getIndex($indexName);
         $type = $index->getType($typeName);
 
         return $type->getMapping();
@@ -417,7 +406,6 @@ class ElasticaAdaptor implements AdaptorInterface
      */
     public function getIndexMapping($indexName)
     {
-        Assertion::string($indexName, 'indexName has to be a string!');
 
         $client = $this->getClient();
         $index = $client->getIndex($indexName);
